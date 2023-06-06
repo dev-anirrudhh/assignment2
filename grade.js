c2425b8d00695feb3b54ef73a015741b7055fe9c
dev.anirrudhh <anirrudhhbabu@gmail.com>
2023-06-07 01:56:12 +0530

    first commit

---
+++ grade.js
@@ -0,0 +1,5 @@
+let i=78;
+if(i>90){console.log("grade A");}
+else if(i>70){console.log("grade B");}
+else if(i>50){console.log("grade C");}
+else if(i>30){console.log("grade D");}
---
+++ squire.js
@@ -0,0 +1,5 @@
+let i;
+ const b=[1,3,9,12,18,21];
+ for (i=0;i<=6;i++){
+   console.log( b[i],"squire =",b[i]**2);
+ }
